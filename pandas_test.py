f58d4d7488105eb46850e2dc9bb36a37a0630ff6
Chris Healy <chrishealy@Chriss-MacBook-Air.local>
2017-02-21 12:27:04 +0000

    Complete Panda with Graph Output

--- pandas_test.py
+++ pandas_test.py
@@ -1,17 +1,19 @@
-import datetime
 import pandas
-from pandas_datareader import data
 import matplotlib.pyplot as plt
 from matplotlib import style
 
 style.use('fivethirtyeight')
 
-start = datetime.datetime(2010, 1, 1)
-end = datetime.datetime(2015, 8, 22)
+web_stats = {'Day': [1, 2, 3, 4, 5, 6],
+             'Visitors': [43, 34, 65, 56, 29, 76],
+             'Bounce Rate': [65, 67, 78, 65, 45, 52]}
 
-df = data.DataReader("XOM", "yahoo", start, end)
+df = pandas.DataFrame(web_stats)
 
-print df.head()
-df['High'].plot()
-plt.legend()
+new_df = df.set_index('Day')
+
+df.plot()
 plt.show()
+
+print df[['Visitors','Bounce Rate']]
+
